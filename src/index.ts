fc2c27abf8f4a177c115e7e19f3adf176709e8b2
LeThanhDat <Le Thanh Dat>
2023-10-26 09:38:54 +0700

    parse mp3

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 import { startSession } from "./config/postgres";
 import { idToMusic } from "./config/constants";
 import { serVerFetch } from "./model/helper/fetchHelper";
+import NodeID3 from "node-id3";
 
 import configApp from "./config";
 const app: Application = express();
@@ -14,15 +15,26 @@ app.get("/", async (req: Request, res: Response) => {
 });
 
 app.get("/stream", (req, res) => {
-  fs.readFile(idToMusic("UxxajLWwzqY"), (err, buffer) => {
+  // fs.readFile(idToMusic("UxxajLWwzqY"), (err, buffer) => {
+  //   if (err) return res.send(err);
+  //   res.send(buffer);
+  // });
+  // fs.readFile(idToMusic("UxxajLWwzqY"), "utf-8", (err, data) => {
+  //   if (err) return res.send(err);
+  //   const header = []
+  //   for(let i=0;i<500;i++) header.push(data[i])
+  //   res.send(header);
+  // });
+  NodeID3.read(idToMusic("UxxajLWwzqY"), (err : any, tags : any) => {
     if (err) return res.send(err);
-    res.send(buffer);
+    return res.send(tags)
   });
 });
 
 app.get("/stream:id", (req, res) => {
   fs.readFile(idToMusic(req.params.id), (err, buffer) => {
     if (err) return res.send(err);
+    console.log(JSON.stringify(buffer));
     res.send(buffer);
   });
 });
@@ -34,3 +46,4 @@ app.listen(3000, async () => {
 // get all id by checking the the data folder, if id post is not in => Fail/download?
 // middleware check session, allow post auth + post user
 // user table + music playlist with user id and music id + music infor that contain the youtube id + other id => 3 table + session === 4
+// will make 2 function call to get stream + infor
