21ccf0af6e12180f02e48f4bdbe5cd007e4c3f41
LeThanhDat <Le Thanh Dat>
2023-10-27 09:47:45 +0700

    check to block song longer than 5 min Use search youube to ge infors, Use regex for duration

--- src/model/Music/index.ts
+++ src/model/Music/index.ts
@@ -1,27 +1,43 @@
 import * as yt from "youtube-search-without-api-key";
-import { downloadMusic, musicInforFromId,downloadMusicQueue } from "../helper/fetchHelper";
-import { allMusicId } from "../../config/constants";
+import {
+  downloadMusic,
+  musicInforFromId,
+  downloadMusicQueue,
+} from "../helper/fetchHelper";
+import { allMusicId, regex5minLess } from "../../config/constants";
 /**
  * Given a search query, searching on youtube
  * @param {string} search value (string or videoId).
  */
 const musicSearch = async (search: string) => {
-  console.log(search);
   const videos = await yt.search(search);
+  const result = videos.filter((el) => regex5minLess.test(el.duration_raw));
   //   const videos = await yt.search("y5kIrbG2gRc");
-  return videos;
+  return result;
 };
 
-const downloadMusicCheck = (id: string) => {
+const checkDuration = async (id: string) => {
+  const videosIdArr = await yt.search(id);
+  if (!regex5minLess.test(videosIdArr[0].duration_raw)) return false;
+  return true;
+};
+
+const downloadMusicCheck = async (id: string) => {
+  const check = await checkDuration(id);
+  if (!check)
+    return {
+      success: false,
+      message: "Duration must be less than 5 minutes",
+    };
   const allId = allMusicId();
   if (!allId.includes(id)) return downloadMusic(id);
   return musicInforFromId(id);
 };
 
-const downloadMusicCheckQueue = (id: string) => {
-    const allId = allMusicId();
-    if (!allId.includes(id)) return downloadMusicQueue(id);
-  };
-  
+const downloadMusicCheckQueue = async(id: string) => {
+  const check = await checkDuration(id);
+  const allId = allMusicId();
+  if (!allId.includes(id) && check) return downloadMusicQueue(id);
+};
 
-export { musicSearch ,downloadMusicCheck, downloadMusicCheckQueue};
+export { musicSearch, downloadMusicCheck, downloadMusicCheckQueue };
